b28dd22d198dc898684a9307f4b9706c1e8e62f3
Alesandro Diogo <alesandromendesdiogo24@gmail.com>
2023-12-27 10:42:37 +0100

    Add project files.

---
+++ Post/Program.cs
@@ -0,0 +1,40 @@
+﻿using Post.Entities;
+
+
+namespace Post
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Comment c1 = new Comment("Have a nice trip!");
+            Comment c2 = new Comment("Wow that's awesome!");
+
+            Posts p1 = new Posts(
+                DateTime.Parse("21/06/2018 13:05:44"),
+                "Traveling to  New Zeland",
+                "I'm going to this wonderful country!",
+                12
+                );
+
+            p1.AddComment(c1);
+            p1.AddComment(c2);
+
+            Comment c3 = new Comment("Good night");
+            Comment c4 = new Comment("May the Force be with you");
+
+            Posts p2 = new Posts(
+                DateTime.Parse("28/07/2018 23:14:19"),
+                "Good night guys",
+                "See you tomorrow",
+                5
+                );
+
+            p2.AddComment(c3);
+            p2.AddComment(c4);
+
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+        }
+    }
+}
